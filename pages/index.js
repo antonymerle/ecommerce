dfdb90a451b8042d8d7af37e411d68fab37330f3
Antony Merle <antony.merle@gmail.com>
2023-04-13 17:56:01 +0200

    getStaticProps & heroBanner component integration

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,34 @@
 import React from "react";
+import { Product, FooterBanner, HeroBanner } from "../components";
+import { client } from "@/lib/client";
 
-const Home = () => {
+const Home = ({ banner, products }) => {
   return (
     <>
-      HeroBanner
-      <div>
+      <HeroBanner heroBanner={banner.length && banner[0]} />
+      {console.log(banner)}
+      <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div>
-        {["Product1", "Product2", "Product3"].map((product) => product)}
+      <div className="products-container">
+        {products.map((product) => product.name)}
       </div>
       Footer
     </>
   );
 };
+
+export async function getStaticProps() {
+  const products = await client.fetch(`*[_type == "product"]`);
+  const banner = await client.fetch(`*[_type == "banner"]`);
+
+  return {
+    props: {
+      products,
+      banner,
+    },
+  };
+}
 
 export default Home;
