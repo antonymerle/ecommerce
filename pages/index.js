609d19d3073c400ad6d867e947a0b081438eb0ef
Antony Merle <antony.merle@gmail.com>
2023-04-13 22:36:08 +0200

    integrate Product

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,19 @@ import { Product, FooterBanner, HeroBanner } from "../components";
 import { client } from "@/lib/client";
 
 const Home = ({ banner, products }) => {
+  console.log(products);
+
   return (
     <>
       <HeroBanner heroBanner={banner.length && banner[0]} />
-      {console.log(banner)}
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {products.map((product) => product.name)}
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
       Footer
     </>
