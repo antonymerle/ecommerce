683b42d1672d25640b0efb139fc8d6cd758577e7
Antony Merle <antony.merle@gmail.com>
2023-04-16 10:05:55 +0200

    implementing useContext

---
+++ context/StateContext.js
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const Context = createContext();
+
+export const stateContext = ({ children }) => {
+  const [showCart, setShowCart] = useState(false);
+  const [cartItems, setCartItems] = useState();
+  const [totalPrice, setTotalPrice] = useState();
+  const [totalQuantities, setTotalQuantities] = useState();
+  const [qty, setQty] = useState(1);
+
+  return (
+    <Context.Provider
+      value={{ showCart, cartItems, totalPrice, totalQuantities, qty }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
