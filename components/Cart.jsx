f1378d699847050ba7aa9132b0b1bf01ee79214b
Antony Merle <antony.merle@gmail.com>
2023-04-16 11:20:03 +0200

    cart closing callback & dynamic quantities

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { useRef } from "react";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiOutlineLeft,
+  AiOutlineShopping,
+} from "react-icons/ai";
+import { TiDeleteOutline } from "react-icons/ti";
+import { Toast } from "react-hot-toast";
+
+import { useStateContext } from "@/context/StateContext";
+import { useNextSanityImage } from "next-sanity-image";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const cartRef = useRef();
+  const { setShowCart, cartItems, totalPrice, totalQuantities } =
+    useStateContext();
+  return (
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button
+          type="button"
+          className="cart-heading"
+          onClick={() => setShowCart(false)}
+        >
+          <AiOutlineLeft />
+          <span className="heading">Your Cart</span>
+          <div className="span cart-num-items">({totalQuantities} items)</div>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
