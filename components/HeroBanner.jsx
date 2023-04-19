c251b18a84aa31100653f7ec0697cfbb43726b27
Antony Merle <antony.merle@gmail.com>
2023-04-19 21:44:16 +0200

    param destructuring

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,27 +1,34 @@
 import Link from "next/link";
 import { urlFor } from "@/lib/client";
 
-const HeroBanner = ({ heroBanner }) => {
+const HeroBanner = ({
+  heroBanner: {
+    smallText,
+    midText,
+    largeText1,
+    product,
+    buttonText,
+    discount,
+    desc,
+    image,
+  },
+}) => {
   return (
     <div className="hero-banner-container">
       <div className="hero-banner-main-text">
-        <p>{heroBanner.smallText}</p>
-        <h3>{heroBanner.midText}</h3>
-        <h1>{heroBanner.largeText1}</h1>
-        <Link href={`/product/${heroBanner.product}`}>
-          <button type="button">{heroBanner.buttonText}</button>
+        <p>{smallText}</p>
+        <h3>{midText}</h3>
+        <h1>{largeText1}</h1>
+        <Link href={`/product/${product}`}>
+          <button type="button">{buttonText}</button>
         </Link>
       </div>
 
       <div className="hero-banner-image-container">
-        <img
-          src={urlFor(heroBanner.image)}
-          className="hero-banner-image"
-          alt={heroBanner.product}
-        />
+        <img src={urlFor(image)} className="hero-banner-image" alt={product} />
         <div className="desc">
-          <h5>{heroBanner.discount}</h5>
-          <p>{heroBanner.desc}</p>
+          <h5>{discount}</h5>
+          <p>{desc}</p>
         </div>
       </div>
     </div>
