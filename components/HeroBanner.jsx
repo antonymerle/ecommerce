3a31ec6dedc5e22b25b0df85e22a3f9d87940469
Antony Merle <antony.merle@gmail.com>
2023-04-19 18:46:11 +0200

    fixed layout for banner large screens

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -4,23 +4,24 @@ import { urlFor } from "@/lib/client";
 const HeroBanner = ({ heroBanner }) => {
   return (
     <div className="hero-banner-container">
-      <div>
-        <p className="beats-solo">{heroBanner.smallText}</p>
+      <div className="hero-banner-main-text">
+        <p>{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
+        <Link href={`/product/${heroBanner.product}`}>
+          <button type="button">{heroBanner.buttonText}</button>
+        </Link>
+      </div>
+
+      <div className="hero-banner-image-container">
         <img
           src={urlFor(heroBanner.image)}
           className="hero-banner-image"
           alt={heroBanner.product}
         />
-        <div>
-          <Link href={`/product/${heroBanner.product}`}>
-            <button type="button">{heroBanner.buttonText}</button>
-          </Link>
-          <div className="desc">
-            <h5>Description</h5>
-            <p>{heroBanner.desc}</p>
-          </div>
+        <div className="desc">
+          <h5>{heroBanner.discount}</h5>
+          <p>{heroBanner.desc}</p>
         </div>
       </div>
     </div>
