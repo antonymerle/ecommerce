b2cb43b9723c28a8045a036101c6d10f4a1b82af
Antony Merle <antony.merle@gmail.com>
2023-04-20 15:25:34 +0200

    favicons

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,6 +7,28 @@ const Layout = ({ children }) => {
     <div className="layout">
       <Head>
         <title>Antony Merle boutique en ligne e-commerce</title>
+        <meta
+          name="description"
+          content="Modèle de boutique ecommerce. Réalisation sur mesure par Antony Merle, développeur web à Anglet, France"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <header>
         <Navbar />
