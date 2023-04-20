df010051c5d946ebf1b2207d9a9bf9892208ba12
Antony Merle <antony.merle@gmail.com>
2023-04-20 16:10:29 +0200

    modularize Navbar CSS

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,22 +2,23 @@ import Link from "next/link";
 import { AiOutlineShopping } from "react-icons/ai";
 import Cart from "./Cart";
 import { useStateContext } from "@/context/StateContext";
+import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
   const { showCart, setShowCart, totalQuantities } = useStateContext();
 
   return (
-    <div className="navbar-container">
-      <p className="logo">
+    <div className={styles.navbarContainer}>
+      <p className={styles.logo}>
         <Link href="/">Artefacts e-commerce</Link>
       </p>
       <button
         type="button"
-        className="cart-icon"
+        className={styles.cartIcon}
         onClick={() => setShowCart(true)}
       >
         <AiOutlineShopping />
-        <span className="cart-item-qty">{totalQuantities}</span>
+        <span className={styles.cartItemQty}>{totalQuantities}</span>
       </button>
 
       {showCart && <Cart />}
