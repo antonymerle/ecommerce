1e7365feafce3b3db6e81ac7e9384e0cdd6ec7d5
Antony Merle <antony.merle@gmail.com>
2023-04-20 11:00:51 +0200

    flexbox for footer banner CSS on large screens ok

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -19,22 +19,22 @@ const FooterBanner = ({
 
   return (
     <div className="footer-banner-container">
-      <div className="banner-desc">
-        <div className="left">
-          <p>{discount}</p>
-          <h3>{largeText1}</h3>
-          <h3>{largeText2}</h3>
-          <p>{saleTime}</p>
-        </div>
-        <div className="right">
-          <p>{smallText}</p>
-          <h3>{midText}</h3>
-          <p>{desc}</p>
-          <Link href={`/product/${product}`}>
-            <button type="button">{buttonText}</button>
-          </Link>
-        </div>
+      <div className="footer-main-text-container">
+        <p>{discount}</p>
+        <h3>{largeText1}</h3>
+        <h3>{largeText2}</h3>
+        <p>{saleTime}</p>
+      </div>
+      <div className="footer-image-container">
         <img src={urlFor(image)} className="footer-banner-image" />
+      </div>
+      <div className="footer-secondary-text-container">
+        <p>{smallText}</p>
+        <h3>{midText}</h3>
+        <p>{desc}</p>
+        <Link href={`/product/${product}`}>
+          <button type="button">{buttonText}</button>
+        </Link>
       </div>
     </div>
   );
