9cd70dbfc87b69f1659a3b67ff6fddbdb763a097
Antony Merle <antony.merle@gmail.com>
2023-04-20 11:00:17 +0200

    Footler list of links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,10 +6,32 @@ const Footer = () => {
       <p>
         {new Date().getFullYear()} Artefacts e-commerce. Tous droits réservés.
       </p>
-      <p className="icons">
-        <AiFillInstagram />
-        <AiOutlineTwitter />
-      </p>
+      <ul className="icons">
+        <li>
+          <a
+            href="https://www.instagram.com/antonymerle"
+            target="blank"
+            rel="noopener noreferrer"
+            aria-label="Antony Merle's Instagram profile"
+          >
+            <div>
+              <AiFillInstagram />
+            </div>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://twitter.com/AntonyMerleDev"
+            target="blank"
+            rel="noopener noreferrer"
+            aria-label="Antony Merle's Twitter profile"
+          >
+            <div>
+              <AiOutlineTwitter />
+            </div>
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
