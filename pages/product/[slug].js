96cd3451b89875018f342e8c8f6a299e55e971ab
Antony Merle <antony.merle@gmail.com>
2023-04-20 14:11:43 +0200

    clean code

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -12,9 +12,7 @@ import { Product } from "@/components";
 
 const ProductDetails = ({ product, products }) => {
   const [index, setIndex] = useState(0);
-
   const { qty, incQty, decQty, onAdd, setShowCart } = useStateContext();
-
   const { image, name, details, price } = product;
 
   const handleBuyNow = () => {
