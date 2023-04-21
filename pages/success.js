59dbac329de5cdd17b91adacc1eee226b057fa72
Antony Merle <antony.merle@gmail.com>
2023-04-21 16:43:06 +0200

    Component integration

--- pages/success.js
+++ pages/success.js
@@ -1,46 +1,7 @@
-import { useEffect } from "react";
-import Link from "next/link";
-import { BsBagCheckFill } from "react-icons/bs";
-import { useStateContext } from "@/context/StateContext";
-import { runFireworks } from "@/lib/utils";
+import Success from "@/components/Success";
 
-const Success = () => {
-  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
-  useEffect(() => {
-    localStorage.clear();
-    setCartItems([]);
-    setTotalPrice(0);
-    setTotalQuantities(0);
-    runFireworks();
-  }, []);
-
-  return (
-    <div className="success-wrapper">
-      <div className="success">
-        <BsBagCheckFill
-          size="12rem"
-          color="green"
-          style={{ padding: "2rem" }}
-        />
-
-        <h2>Merci pour votre commande !</h2>
-        <p className="email-msg">
-          Un email de confirmation vous a été expédié.
-        </p>
-        <p className="description">
-          Pour toute question, n'hésitez pas à nous écrire à cette adresse :{" "}
-          <a href="mailto:order@example.com" className="email">
-            order@example.com
-          </a>
-        </p>
-        <Link href="/">
-          <button type="btn" width="300px" className="btn">
-            Continuer mes achats
-          </button>
-        </Link>
-      </div>
-    </div>
-  );
+const success = () => {
+  return <Success />;
 };
 
-export default Success;
+export default success;
