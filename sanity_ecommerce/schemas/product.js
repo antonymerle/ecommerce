928eadbf5585025e3ff96235a9d9595a829c3765
Antony Merle <antony.merle@gmail.com>
2023-04-21 17:41:42 +0200

    added promotion & oldPrice to schema

--- sanity_ecommerce/schemas/product.js
+++ sanity_ecommerce/schemas/product.js
@@ -15,6 +15,8 @@ export default {
     {name: 'name', title: 'Name', type: 'string'},
     {name: 'slug', title: 'Slug', type: 'slug', options: {source: 'name', maxLength: 90}},
     {name: 'price', title: 'Price', type: 'number'},
+    {name: 'promotion', title: 'Promotion', type: 'number'},
+    {name: 'oldPrice', title: 'Old Price', type: 'number'},
     {name: 'details', title: 'Details', type: 'string'},
   ],
 }
