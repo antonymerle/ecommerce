d1823b73803f546b6ca1115324f0a2cc0e8db629
Antony Merle <antony.merle@gmail.com>
2023-04-22 10:52:13 +0200

    importing changes on auth branch

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,20 @@ import "@/styles/globals.css";
 import Layout from "../components/Layout";
 import { StateContext } from "@/context/StateContext";
 import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <StateContext>
-      <Layout>
-        <Toaster />
-        <Component {...pageProps} />
-      </Layout>
-    </StateContext>
+    <SessionProvider session={session}>
+      <StateContext>
+        <Layout>
+          <Toaster />
+          <Component {...pageProps} />
+        </Layout>
+      </StateContext>
+    </SessionProvider>
   );
 }
