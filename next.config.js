31db9676f6a55340238dade8b8f857e1a9fefee4
Antony Merle <antony.merle@gmail.com>
2023-04-22 15:22:51 +0200

    auth : Facebook provider integration

--- next.config.js
+++ next.config.js
@@ -14,6 +14,8 @@ const nextConfig = {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GOOGLE_ID: process.env.GOOGLE_ID,
     GOOGLE_SECRET: process.env.GOOGLE_SECRET,
+    FACEBOOK_CLIENT_ID: process.env.FACEBOOK_CLIENT_ID,
+    FACEBOOK_CLIENT_SECRET: process.env.FACEBOOK_CLIENT_SECRET,
   },
 };
 
