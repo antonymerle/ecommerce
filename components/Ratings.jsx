31e19ce2ff816e2216089cfe38d955a9508270d9
Antony Merle <antony.merle@gmail.com>
2023-04-24 16:48:02 +0200

    rating logic impl

--- components/Ratings.jsx
+++ components/Ratings.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MdStar, MdStarOutline, MdStarHalf } from "react-icons/md";
 import { computeMean } from "@/lib/utils";
 import style from "../styles/Ratings.module.css";
 
 const { ratings, gold } = style;
+
+// TODO : rec user rating in local state and account
+// TODO : rec user rating in DB
 
 const Ratings = ({ product }) => {
   const [isStarHovered, setIsStarHovered] = useState([
@@ -14,6 +17,16 @@ const Ratings = ({ product }) => {
     false,
   ]);
 
+  const [userStars, setUserStars] = useState([]);
+
+  useEffect(() => {
+    setUserStars([]);
+  }, [product.slug]);
+
+  const mean = computeMean(product.ratings);
+  const STAR_MAX = 5;
+  let starsFromBackend = [];
+
   // turn stars to gold or returns them in their original state
   const midas = (index) => {
     return isStarHovered.map((star, i) => {
@@ -23,13 +36,20 @@ const Ratings = ({ product }) => {
     });
   };
 
-  const mean = computeMean(product.ratings);
-  const STAR_MAX = 5;
-  let stars = [];
+  const handleRate = (index) => {
+    const result = [];
+    for (let i = 0; i < STAR_MAX; i++) {
+      if (i <= index) result.push(true);
+      else {
+        result.push(false);
+      }
+      setUserStars(result);
+    }
+  };
 
   for (let i = 0; i < STAR_MAX; i++) {
     if (i < mean && mean < i + 1) {
-      stars.push(
+      starsFromBackend.push(
         <MdStarHalf
           onMouseOver={() => {
             setIsStarHovered(midas(i));
@@ -37,12 +57,15 @@ const Ratings = ({ product }) => {
           onMouseLeave={() =>
             setIsStarHovered([false, false, false, false, false])
           }
+          onClick={() => {
+            handleRate(i);
+          }}
           key={i}
           color={isStarHovered[i] ? "gold" : ""}
         />
       );
     } else if (i < mean) {
-      stars.push(
+      starsFromBackend.push(
         <MdStar
           onMouseOver={() => {
             setIsStarHovered(midas(i));
@@ -50,12 +73,15 @@ const Ratings = ({ product }) => {
           onMouseLeave={() =>
             setIsStarHovered([false, false, false, false, false])
           }
+          onClick={() => {
+            handleRate(i);
+          }}
           key={i}
           color={isStarHovered[i] ? "gold" : ""}
         />
       );
     } else {
-      stars.push(
+      starsFromBackend.push(
         <MdStarOutline
           onMouseOver={() => {
             setIsStarHovered(midas(i));
@@ -63,6 +89,9 @@ const Ratings = ({ product }) => {
           onMouseLeave={() =>
             setIsStarHovered([false, false, false, false, false])
           }
+          onClick={() => {
+            handleRate(i);
+          }}
           key={i}
           color={isStarHovered[i] ? "gold" : ""}
         />
@@ -72,7 +101,40 @@ const Ratings = ({ product }) => {
 
   return (
     <div className={ratings}>
-      <div>{stars}</div>
+      <div>
+        {userStars.length
+          ? userStars.map((star, i) =>
+              star ? (
+                <MdStar
+                  onMouseOver={() => {
+                    setIsStarHovered(midas(i));
+                  }}
+                  onMouseLeave={() =>
+                    setIsStarHovered([false, false, false, false, false])
+                  }
+                  onClick={() => {
+                    handleRate(i);
+                  }}
+                  key={i}
+                  color={"gold"}
+                />
+              ) : (
+                <MdStarOutline
+                  onMouseOver={() => {
+                    setIsStarHovered(midas(i));
+                  }}
+                  onMouseLeave={() =>
+                    setIsStarHovered([false, false, false, false, false])
+                  }
+                  onClick={() => {
+                    handleRate(i);
+                  }}
+                  key={i}
+                />
+              )
+            )
+          : starsFromBackend}
+      </div>
       <p>{computeMean(product.ratings)}</p>
     </div>
   );
