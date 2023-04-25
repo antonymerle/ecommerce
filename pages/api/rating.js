9fdc06ee7bb2130cdc7cf025501d7b3839e15989
Antony Merle <antony.merle@gmail.com>
2023-04-26 00:03:39 +0200

    recieve body parameters & TODO

--- pages/api/rating.js
+++ pages/api/rating.js
@@ -4,7 +4,12 @@ import { authOptions } from "./auth/[...nextauth]";
 export default async (req, res) => {
   const session = await getServerSession(req, res, authOptions);
   console.log({ session });
-  if (session) {
+  if (session && req.method === "POST") {
+    const { starsNumber, productId } = req.body;
+    console.log(starsNumber, productId);
+
+    // TODO: Come up with a query to update user rated products based on user session, product id and starsNumber.
+
     // Signed in
     res.json({ result: true, user: session.user.name });
   } else {
