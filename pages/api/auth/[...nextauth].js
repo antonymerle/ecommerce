be4016bfa594d1788e96a63023b993277c1db9c9
Antony Merle <antony.merle@gmail.com>
2023-04-27 00:13:00 +0200

    begin impl CredentialsProvider logic

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 import { createClient } from "next-sanity";
 
@@ -23,9 +24,69 @@ export const authOptions = {
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "email",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "john.doe@gmail.com",
+        },
+        password: { label: "Mot de passe", type: "password" },
+      },
+      async authorize(credentials, req) {
+        console.log({ credentials }, { req: req.body });
+
+        const groqQuery = `*[_type == "user" && email == $userEmail]`;
+
+        const userinDB = await client.fetch(groqQuery, {
+          userEmail: req.body.email,
+        });
+
+        console.log({ userinDB });
+
+        let user = null;
+
+        if (userinDB?.length) {
+          console.log("There already someone with that email in DB");
+          user = {
+            id: userinDB[0]._id,
+            name: `${userinDB[0].given_name} ${userinDB[0].family_name}`,
+            email: userinDB[0].email,
+          };
+        }
+
+        // Add logic here to look up the user from the credentials supplied
+        // const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+
+        if (user) {
+          console.log("ok branch");
+          console.log(user);
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          console.log("no-no branch");
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
   ],
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
+      console.log("signin callback");
+      if (credentials) {
+        console.log("user signed in with credential", { user });
+        // HANDLE signin with credentials
+        return credentials;
+      }
       const doc = {
         _id: account.providerAccountId,
         _type: "user",
