644b1822f2a79b428648b55d42c5a3d17cb93f5f
Antony Merle <antony.merle@gmail.com>
2023-04-29 17:02:04 +0200

    credential sign-in endpoint

---
+++ pages/api/auth/credentials-signin.js
@@ -0,0 +1,38 @@
+import bcrypt from "bcrypt";
+import { client } from "./[...nextauth]";
+
+export default async (req, res) => {
+  const { email, password, formType } = req.body;
+  console.log("signin endpoint reached");
+  // console.log({ body: req.body });
+
+  if (req.method !== "POST" || !email || !password || formType !== "signin") {
+    console.log("Wrong request");
+
+    return res.status(400).json({ result: false, error: "Wrong request" });
+  }
+
+  const groqQuery = `*[_type == "user" && email == $userEmail]`;
+
+  const userinDB = await client.fetch(groqQuery, {
+    userEmail: email,
+  });
+
+  console.log(userinDB);
+
+  if (userinDB?.length) {
+    console.log("User found in DB, checking password");
+    if (bcrypt.compareSync(password, userinDB[0].password ?? "")) {
+      console.log("passwords hashes match !");
+      return res.status(200).json(userinDB[0]);
+    } else {
+      console.log("Passwords DO NOT match !");
+
+      return res
+        .status(403)
+        .json({ result: false, error: "Wrong credentials" });
+    }
+  }
+
+  res.status(400).json({ result: false, error: "User not found." });
+};
