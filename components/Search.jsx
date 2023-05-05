71a583949e16ed42a4ec02619ce1a25aa7fa59de
Antony Merle <antony.merle@gmail.com>
2023-05-06 00:11:32 +0200

    Search component implementation

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,12 +1,37 @@
+import { useState } from "react";
 import Product from "./Product";
+import { BsSearch } from "react-icons/bs";
+import style from "../styles/Search.module.css";
+
+const { searchResults, container, searchBox, inputContainer } = style;
 
 const Search = ({ products }) => {
-  console.log(products);
-  const results = products?.map((product) => {
-    return <Product key={product._id} product={product} />;
-  });
+  const [search, setSearch] = useState("");
 
-  return <div>{results}</div>;
+  const results =
+    search.length > 1
+      ? products
+          ?.filter((product) =>
+            product.name.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((product) => {
+            return <Product key={product._id} product={product} />;
+          })
+      : null;
+
+  return (
+    <div className={container}>
+      <div className={inputContainer}>
+        <input
+          className={searchBox}
+          placeholder="Rechercher dans la boutique"
+          onChange={(e) => setSearch(e.target.value)}
+        ></input>
+        <BsSearch size={"30px"} color={"#324d67"} />
+      </div>
+      <div className={searchResults}>{results}</div>
+    </div>
+  );
 };
 
 export default Search;
