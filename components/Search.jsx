722c4f9bc209a4dcfe868804eb8354f4cddf805f
Antony Merle <antony.merle@gmail.com>
2023-05-05 22:58:46 +0200

    display products

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,5 +1,12 @@
-const Search = () => {
-  return <div>Search</div>;
+import Product from "./Product";
+
+const Search = ({ products }) => {
+  console.log(products);
+  const results = products?.map((product) => {
+    return <Product key={product._id} product={product} />;
+  });
+
+  return <div>{results}</div>;
 };
 
 export default Search;
