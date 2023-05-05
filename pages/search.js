1a9c4ef32d6633bde8bc21dfc23d91bf5ca6695c
Antony Merle <antony.merle@gmail.com>
2023-05-05 22:58:37 +0200

    getServerSideProps

--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,20 @@
+import { client } from "@/lib/client";
 import Search from "@/components/Search";
 
-const search = () => {
-  return <Search />;
+const search = ({ products }) => {
+  return <Search products={products} />;
 };
+
+export async function getServerSideProps(context) {
+  const products = await client.fetch(
+    `*[_type == "product" && !(_id in path("drafts.**"))]`
+  );
+
+  return {
+    props: {
+      products,
+    },
+  };
+}
 
 export default search;
