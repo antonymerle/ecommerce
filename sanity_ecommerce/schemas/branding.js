8e7f27dd5bc314787d8f84412f5d1000d1ce1038
Antony Merle <antony.merle@gmail.com>
2023-05-12 07:05:29 +0200

    branding schema

---
+++ sanity_ecommerce/schemas/branding.js
@@ -0,0 +1,31 @@
+export default {
+  name: 'branding',
+  title: 'Branding',
+  type: 'document',
+  fields: [
+    {
+      name: 'logoNavbar',
+      title: 'Logo Navbar',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'logoFull',
+      title: 'Logo Full Size',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'checkoutImage',
+      title: 'Checkout Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+}
--- sanity_ecommerce/schemas/index.js
+++ sanity_ecommerce/schemas/index.js
@@ -1,6 +1,7 @@
+import branding from './branding'
 import footerBanner from './footerBanner'
 import product from './product'
 import upperBanner from './upperBanner'
 import user from './user'
 
-export const schemaTypes = [product, upperBanner, footerBanner, user]
+export const schemaTypes = [product, upperBanner, footerBanner, user, branding]
