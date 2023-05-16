53d17018a9cf4d56c82f6033ba1ca7a9760501df
Antony Merle <antony.merle@gmail.com>
2023-05-16 21:07:27 +0200

    impl Heart with backend fetch

--- components/Heart.jsx
+++ components/Heart.jsx
@@ -1,12 +1,35 @@
+import { useEffect, useState } from "react";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { useStateContext } from "@/context/StateContext";
 
-const Heart = ({ isProductFaved }) => {
-  console.log({ isProductFaved });
-  !isProductFaved ?? true;
+const Heart = ({ productId }) => {
+  const [isProductFaved, setIsProductFaved] = useState(false);
+  const { userSession, userFavs } = useStateContext();
+
+  useEffect(() => {
+    const checkIfProducIsFaved =
+      userFavs.filter((p) => p._id === productId).length === 1;
+    setIsProductFaved(checkIfProducIsFaved);
+  }, [userFavs, userSession]);
+
+  const handleRequest = () => {
+    fetch("/api/favorite", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        productId,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
+  console.log({ productId });
+
   return isProductFaved ? (
-    <FaHeart onClick={() => console.log("unfaved")} />
+    <FaHeart onClick={handleRequest} />
   ) : (
-    <FaRegHeart onClick={() => console.log("faved")} />
+    <FaRegHeart onClick={handleRequest} />
   );
 };
 
