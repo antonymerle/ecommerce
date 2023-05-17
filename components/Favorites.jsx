5167b65236c9c0b5e379025266e6384d80e88563
Antony Merle <antony.merle@gmail.com>
2023-05-17 14:02:53 +0200

    Favorites component impl

--- components/Favorites.jsx
+++ components/Favorites.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { useStateContext } from "@/context/StateContext";
+import Product from "./Product";
 
-const Favorites = () => {
-  return <div>Favorites</div>;
+const Favorites = ({ userFavoritesProducts, userRatedProducts }) => {
+  // const [first, setfirst] = useState(second)
+  console.log({ userFavoritesProducts });
+  const { userSession, userRatings } = useStateContext();
+
+  return (
+    <div className="products-container">
+      {userFavoritesProducts?.map((product) => (
+        <Product
+          key={product._id}
+          product={product}
+          userRatedProducts={userRatedProducts}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Favorites;
