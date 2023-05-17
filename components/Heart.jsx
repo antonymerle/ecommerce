3519bf5b9eef5f12e8dc155caf448ffc97f31c05
Antony Merle <antony.merle@gmail.com>
2023-05-17 11:26:03 +0200

    favorites wiring from context and server

--- components/Heart.jsx
+++ components/Heart.jsx
@@ -4,15 +4,25 @@ import { useStateContext } from "@/context/StateContext";
 
 const Heart = ({ productId }) => {
   const [isProductFaved, setIsProductFaved] = useState(false);
-  const { userSession, userFavs } = useStateContext();
+  const {
+    userSession,
+    userFavs,
+    updateUserFavorites,
+    addUserFavorite,
+    removeUserFavorite,
+  } = useStateContext();
 
   useEffect(() => {
-    const checkIfProducIsFaved =
-      userFavs.filter((p) => p._id === productId).length === 1;
-    setIsProductFaved(checkIfProducIsFaved);
-  }, [userFavs, userSession]);
+    console.log({ userFavs, productId });
+    setIsProductFaved(isFaved(productId));
+  }, []);
+
+  const isFaved = (productId) => {
+    return userFavs.filter((p) => p === productId).length === 1;
+  };
 
   const handleRequest = () => {
+    setIsProductFaved(!isFaved(productId)); // Avoid latency from waiting server response
     fetch("/api/favorite", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -21,10 +31,17 @@ const Heart = ({ productId }) => {
       }),
     })
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        console.log(data);
+        if (data.faved === true) {
+          addUserFavorite(productId);
+        } else {
+          removeUserFavorite(productId);
+        }
+      });
   };
 
-  console.log({ productId });
+  // console.log({ productId });
 
   return isProductFaved ? (
     <FaHeart onClick={handleRequest} />
