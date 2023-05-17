aa344d7ba38b79649c18bb6c0592c31807e89d42
Antony Merle <antony.merle@gmail.com>
2023-05-17 13:28:42 +0200

    complete rehaul

--- components/Heart.jsx
+++ components/Heart.jsx
@@ -2,25 +2,23 @@ import { useEffect, useState } from "react";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { useStateContext } from "@/context/StateContext";
 
-const Heart = ({ productId }) => {
-  const [isProductFaved, setIsProductFaved] = useState(false);
-  const {
-    userSession,
-    userFavs,
-    updateUserFavorites,
-    addUserFavorite,
-    removeUserFavorite,
-  } = useStateContext();
-
+const isFaved = (userFavs, productId) => {
   console.log({ userFavs, productId });
+  const result = userFavs.includes(productId);
+  console.log({ result });
+  return result;
+};
+
+const Heart = ({ userFavs, productId }) => {
+  const [isProductFaved, setIsProductFaved] = useState(false);
+  const { addUserFavorite, removeUserFavorite } = useStateContext();
+
+  // console.log({ userFavs, productId });
 
   useEffect(() => {
-    setIsProductFaved(isFaved(productId));
-  }, []);
-
-  const isFaved = (productId) => {
-    return userFavs.filter((p) => p === productId).length === 1;
-  };
+    // console.log(isFaved(userFavs, productId));
+    setIsProductFaved(isFaved(userFavs, productId));
+  }, [productId, userFavs]);
 
   const handleRequest = () => {
     setIsProductFaved(!isFaved(productId)); // Avoid latency from waiting server response
