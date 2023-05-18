04ea8bfeca699d8b31e18f8cfb03b5565fa8f53c
Antony Merle <antony.merle@gmail.com>
2023-05-18 10:38:02 +0200

    title

--- components/Favorites.jsx
+++ components/Favorites.jsx
@@ -1,5 +1,8 @@
 import { useStateContext } from "@/context/StateContext";
 import ProductsContainer from "./ProductsContainer";
+import style from "../styles/Favorites.module.css";
+
+const { container, title } = style;
 
 const Favorites = ({ userFavoritesProducts, userRatedProducts }) => {
   // const [first, setfirst] = useState(second)
@@ -7,10 +10,13 @@ const Favorites = ({ userFavoritesProducts, userRatedProducts }) => {
   const { userSession, userRatings } = useStateContext();
 
   return (
-    <ProductsContainer
-      productsArray={userFavoritesProducts}
-      userRatedProducts={userRatedProducts}
-    />
+    <div className={container}>
+      <h1 className={title}>Mes favoris</h1>
+      <ProductsContainer
+        productsArray={userFavoritesProducts}
+        userRatedProducts={userRatedProducts}
+      />
+    </div>
   );
 };
 
