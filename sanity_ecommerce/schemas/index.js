fb0bc7a76eeb980ead60400375f45db012cfea9c
Antony Merle <antony.merle@gmail.com>
2023-05-19 16:45:09 +0200

    include order schema

--- sanity_ecommerce/schemas/index.js
+++ sanity_ecommerce/schemas/index.js
@@ -3,5 +3,6 @@ import footerBanner from './footerBanner'
 import product from './product'
 import upperBanner from './upperBanner'
 import user from './user'
+import order from './order'
 
-export const schemaTypes = [product, upperBanner, footerBanner, user, branding]
+export const schemaTypes = [product, upperBanner, footerBanner, user, branding, order]
