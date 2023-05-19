b45d8451640511f3585366f5e446685dbc273c8f
Antony Merle <antony.merle@gmail.com>
2023-05-19 16:43:45 +0200

    added wrapper & style

--- components/Orders.jsx
+++ components/Orders.jsx
@@ -1,7 +1,15 @@
 import DataTable from "./DataTable";
+import style from "../styles/Orders.module.css";
+
+const { container, title } = style;
 
 const Orders = () => {
-  return <DataTable />;
+  return (
+    <div className={container}>
+      <h1 className={title}>Mes commandes</h1>
+      <DataTable />
+    </div>
+  );
 };
 
 export default Orders;
